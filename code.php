78ed69780ac38757d79059e8d61e3280a19a8b09
hannahrajput <78543289+hannahrajput@users.noreply.github.com>
2021-02-10 15:49:53 +0530

    Add files via upload

---
+++ code.php
@@ -0,0 +1,36 @@
+<?php
+include ("dbConfig.php");
+
+// if(isset($_POST['butsignup'])){
+    $Username = $_POST['Username'];
+    
+    $Password = $_POST['Password'];
+    $Confirmpassword = $_POST['Confirmpassword'];
+    $Email = $_POST['Email'];
+
+    $data = [
+        'Username'=> $Username,
+        'Email' => $Email,
+        'Password' => $Password,
+        'Confirmpassword' => $Confirmpassword
+        
+    ];
+    
+    // $ref = "contact/";
+    // $postdata = $db->collection($ref)->document($ref)->create($data);
+    
+    // if($postdata){
+    //     echo '<script>alert("data inserted")</script>';
+    // }
+    $docRef = $db->collection('users')->document('details');
+    $docRef->set($data);
+    // printf('Added data to the lovelace document in the users collection.' . PHP_EOL);
+      echo 1;
+// }
+// else{
+//     echo 0;
+// }
+
+
+
+?>
